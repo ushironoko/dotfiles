95d34cf50a34b391df6291a1cce9ec45100438bd
ushironoko <apple19940820@gmail.com>
2025-12-13 13:45:12 +0900

    fix claude settings

--- claude/.claude/hooks/pre_tool_use/no_default_web_fetch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Never can use Default WebFetch and WebSearch. Can Only use mcp__gemini-google-search or mcp__readability. Use gemini and readability to perform follow-up tasks. Don't ask the user to do it, just keep doing it." 1>&2
-exit 2
